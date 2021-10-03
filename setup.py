ff9f0ac089dd924a2ad4c8e6f716626770c99ca5
rcallicotte <rcallicotte@gmail.com>
2021-10-03 09:09:27 -0500

    Added files for learning branch

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
-"""
-Setup for installctl application
-"""
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='installctl',
     version='0.1.0',
-    packages=find_packages(),
-    include_package_data=True,
+    py_modules=['installctl'],
     install_requires=[
         'Click',
     ],
     entry_points={
         'console_scripts': [
-            'installctl = installctl.scripts.installctl:cli',
+            'installctl = installctl:cli',
         ],
     },
 )
